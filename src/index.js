4656adf852bc3aba66023fcd855a2c687948d605
reguv760 <reguv760@gmail.com>
2019-09-23 16:44:10 -0700

    update index

--- src/index.js
+++ src/index.js
@@ -14,4 +14,6 @@ ReactDOM.render(<CssBaseline><App/></CssBaseline>, document.getElementById('root
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+
+//hrs1.5
 serviceWorker.unregister();
