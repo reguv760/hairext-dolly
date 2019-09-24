0879dee1866bd2730ba15913b82dc27cd1c146f6
reguv760 <reguv760@gmail.com>
2019-09-24 15:36:46 -0700

    add strucutre for BeforeAfter component in Gallery section

---
+++ src/components/BeforeAfter.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class BeforeAfter extends React.Component 
+{
+    render()
+    {
+        return(
+            <div className="beforeAfter">
+                <ul className="beforeAfter-container">
+                    <li>
+                        
+                    </li>
+                    <li>
+                        
+                    </li>
+                    <li>
+                        
+                    </li>
+                    <li>
+                        
+                    </li>
+                    <li>
+                        
+                    </li>
+                    <li>
+                        
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default BeforeAfter;
--- src/section/Gallery.js
+++ src/section/Gallery.js
@@ -2,17 +2,19 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import ScrollableAnchor from 'react-scrollable-anchor'
 
+import BeforeAfter from './../components/BeforeAfter';
+
 const GallerySection = () =>
 {
     return (
         <ScrollableAnchor id={'About'}>
             <section>
-            <Container className="section-container">
+            <Container className="section-container" >
                     <div className="section-container__text">
                         <h1>Gallery section</h1>
                         <p>Excepteur enim et magna reprehenderit. Qui exercitation fugiat consectetur officia qui culpa dolor non laborum tempor mollit. Proident magna minim dolor amet. Lorem veniam consectetur elit incididunt fugiat aliqua aute ex dolore dolore nulla culpa. Irure voluptate aute et commodo anim officia enim ad fugiat irure nostrud tempor do dolor. Minim ex qui enim voluptate Lorem nulla occaecat do.</p>
 
-                        <p>Occaecat mollit adipisicing ea eu fugiat nulla veniam voluptate nisi velit voluptate. Reprehenderit aliquip est consectetur eiusmod ut id. Dolore eu id ut esse irure laboris velit ex fugiat occaecat Lorem sunt esse. Ut exercitation laboris mollit cupidatat ea aliquip. Sunt anim consequat ullamco reprehenderit eu aliqua laborum nulla adipisicing velit aliqua nostrud aliqua.</p>
+                        {/* < BeforeAfter /> */}
                     </div>
                     
                 </Container>
