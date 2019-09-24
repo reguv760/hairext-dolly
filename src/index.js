db2b8c98b4fc7f4a904ad5948e7dcdee63869baa
reguv760 <reguv760@gmail.com>
2019-09-24 08:02:21 -0700

    media query on section

--- src/index.js
+++ src/index.js
@@ -14,5 +14,5 @@ ReactDOM.render(<CssBaseline><App/></CssBaseline>, document.getElementById('root
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 
-//hrs3.5
+//hrs4
 serviceWorker.unregister();
--- src/section/Home.js
+++ src/section/Home.js
@@ -10,9 +10,12 @@ const HomeSection = () =>
         
         <ScrollableAnchor id={'Home'}>
             <section>
-                <Container>
-
-                    <h1>Home section</h1>
+                <Container className="section-container">
+                    <div className="section-container__text">
+                        <h1>Home section</h1>
+                        <p>Excepteur enim et magna reprehenderit. Qui exercitation fugiat consectetur officia qui culpa dolor non laborum tempor mollit. Proident magna minim dolor amet. Lorem veniam consectetur elit incididunt fugiat aliqua aute ex dolore dolore nulla culpa. Irure voluptate aute et commodo anim officia enim ad fugiat irure nostrud tempor do dolor. Minim ex qui enim voluptate Lorem nulla occaecat do.</p>
+                    </div>
+                    
                 </Container>
             </section>
         </ScrollableAnchor>
