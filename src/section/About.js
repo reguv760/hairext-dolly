7a248aecdfa979034a48cea894e76720a106d14c
reguv760 <reguv760@gmail.com>
2019-09-26 09:38:39 -0700

    add about section

--- src/section/About.js
+++ src/section/About.js
@@ -10,9 +10,10 @@ const AboutSection = () =>
             <Container className="section-container">
                     <div className="section-container__text">
                         <h1>About</h1>
-                        <p>Excepteur enim et magna reprehenderit. Qui exercitation fugiat consectetur officia qui culpa dolor non laborum tempor mollit. Proident magna minim dolor amet. Lorem veniam consectetur elit incididunt fugiat aliqua aute ex dolore dolore nulla culpa. Irure voluptate aute et commodo anim officia enim ad fugiat irure nostrud tempor do dolor. Minim ex qui enim voluptate Lorem nulla occaecat do.</p>
 
-                        <p>Occaecat mollit adipisicing ea eu fugiat nulla veniam voluptate nisi velit voluptate. Reprehenderit aliquip est consectetur eiusmod ut id. Dolore eu id ut esse irure laboris velit ex fugiat occaecat Lorem sunt esse. Ut exercitation laboris mollit cupidatat ea aliquip. Sunt anim consequat ullamco reprehenderit eu aliqua laborum nulla adipisicing velit aliqua nostrud aliqua.</p>
+                        <p>Laborum nisi cupidatat duis eu aliquip esse ad dolore amet laboris fugiat excepteur eiusmod. Nulla do enim amet tempor cupidatat sint labore excepteur cupidatat voluptate enim elit laborum ex. Fugiat incididunt consectetur ut sunt labore. Ut nostrud eu deserunt do sit dolor duis officia commodo ipsum amet consequat et adipisicing. Velit labore elit ipsum in officia cupidatat laboris ex anim. Tempor non occaecat et labore ullamco dolore non commodo duis adipisicing in anim. Anim sunt irure reprehenderit nisi id non mollit et amet et.
+
+Ut pariatur id enim id non. Officia adipisicing elit do aute aliqua. Sunt sit duis ex laboris tempor laboris tempor enim. Reprehenderit ipsum mollit ullamco reprehenderit et veniam fugiat nulla reprehenderit aliqua sint. Amet excepteur aute ea eiusmod aliqua quis Lorem sunt.</p>
                     </div>
                     
                 </Container>
