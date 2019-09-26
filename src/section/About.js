c21bff4db888688643415f77bc5e7b754f13a358
reguv760 <reguv760@gmail.com>
2019-09-26 12:44:48 -0700

    fix logo div + add images

--- src/section/About.js
+++ src/section/About.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import ScrollableAnchor from 'react-scrollable-anchor'
 
+import DollySalon from './../img/dolly-salon.jpg';
+import DollySelfie from './../img/dolly-selfie.jpg';
+
+
+import CBDPackages from './../img/cbd-packages.jpg';
+
 const AboutSection = () =>
 {
     return (
@@ -11,19 +17,30 @@ const AboutSection = () =>
                     <div className="section-container__text">
                         <h1>About</h1>
 
-                        <p>I have been in the hair styling business for over 20 years and have been using Hairdreams for over 10 years. Over the years, I have been trained and educated in several different areas of hair extensions. After attending several conferences and trade shows, I have found the highest quality hair extensions on the market to be <span className="bold-text"><a href="https://www.hairdreams.com/en-us/" target="_blank" rel="noopener noreferrer">Hairdreams</a>.</span> I strive to meet the needs of my clients and continue to attend trade shows and conferences so that I can continue to give my clients what they deserve: <span className="italic-text">only the best.</span></p>
+                        <div className="about-me">
+                            <div className="col">
+                                <img src={DollySalon} alt="Salon" />
+                            </div>
+                            <div className="col">
+                               
+                                <img src={DollySelfie} alt="Dolly Hoffman selfie" />
+                            </div>
+                        </div>
+
+                        <p>I have been in the hair styling business for over <span className="bold-text">20 years</span> and have been using <span className="bold-text"><a href="https://www.hairdreams.com/en-us/" target="_blank" rel="noopener noreferrer">Hairdreams</a></span> for over <span className="bold-text">10 years.</span> Over the years, I have been trained and educated in several different areas of hair extensions. After attending several conferences and trade shows, I have found the highest quality hair extensions on the market to be <span className="bold-text"><a href="https://www.hairdreams.com/en-us/" target="_blank" rel="noopener noreferrer">Hairdreams</a>.</span> I strive to meet the needs of my clients and continue to attend trade shows and conferences so that I can continue to give my clients what they deserve: <span className="italic-text">only the best.</span></p>
 
                         <p>I believe hair extensions are a true art. Imagine having the hair of your dreams that will not tangle, matt or turn into dreads. I will educate you on home care and coming in for tidy cuts. I will show you the difference in 5-star and 7-star hair. <span className="bold-text"><a href="https://www.hairdreams.com/en-us/" target="_blank" rel="noopener noreferrer">Hairdreams</a></span> is the only hair extension that offers the best European hair.</p>
 
-                        <p>Trust me, the possibilities are endless. Whether you want to add length, volume, color or just upgrade a hairstyle, I can make your dream hair come true!</p>
+                        <p>Trust me, the possibilities are <span className="italic-text">endless!</span>
+                        <br/> Whether you want to add length, volume, color or just upgrade a hairstyle, I can make your dream hair come true!</p>
 
                         
                         <p>I also serve two locations: Buena Park, CA and Mission Viejo, CA.</p>
-
-                        
+                       
 
                         <div className="about-cbd">
                             <div className="col">
+                                <img src={CBDPackages} alt="CBD products" />
                                 <p>I now offer CBD creams and serums that are made with <span className="italic-text">all natural ingredients.</span> They are manufactured by <span className="italic-text">Earthly Body</span>, a trusted manufacturer with quality ingredients since 1996. They are 100% vegan! If you’re living with aches and pains, you will <span className="italic-text">love</span> these products! </p>
 
                                 <p>Also there is <span className="italic-text">no THC</span> in the products!</p>
--- src/section/Services.js
+++ src/section/Services.js
@@ -10,11 +10,11 @@ const ServicesSection = () =>
             <Container className="section-container">
                     <div className="section-container__text">
                         <h1>Services</h1>
-                        <p>I now have 3 options for the person with fine thinning hair. A microline stays in for up to 4 weeks and can be worn while swimming, sweating or showering.</p>                        
+                        <p>I now have 3 options for the person with fine thinning hair. A <span className="italic-text">microline</span> stays in for up to <span className="bold-text">4 weeks</span> and can be worn while swimming, sweating or showering.</p>                        
 
-                        <p>I also offer a <span className="italic-text">highline</span> or <span className="italic-text">top-hair </span>to take care of any hairloss issues. A top-up is done after 4 weeks to tighten up your hairpiece. You can combine a microline with extensions to make your hair beautiful, thick and luxurious for the ultimate combination!</p>
+                        <p>I also offer a <span className="italic-text">highline</span> or <span className="italic-text">top-hair </span>to take care of any hairloss issues. A top-up is done after <span className="bold-text">4 weeks</span> to tighten up your hairpiece. You can combine a microline with extensions to make your hair beautiful, thick and luxurious for the ultimate combination!</p>
 
-                        <p>I also offer <span className="italic-text">Quickies</span>, our new tape-in extensions that stay in for 6-8 weeks. They are the same beautiful hair but a little more affordable for the person who would like a non permanent extension.</p>
+                        <p>I also offer <span className="italic-text">Quickies</span>, our new tape-in extensions that stay in for <span className="bold-text">6-8 weeks</span>. They are the same beautiful hair but a little more affordable for the person who would like a non permanent extension.</p>
 
                         <h2>FAQ</h2>    
                         <h3>What color options do I have?  </h3>  
@@ -24,13 +24,13 @@ const ServicesSection = () =>
                         <p>On average, it takes about <span className="italic-text">3 hours</span> to get it done. Time varies depending on the length of hair, etc.</p>
 
                         <h3>Will it damage my current hair?</h3>    
-                        <p>No, I don't use glue, chemicals or any of the older methods. With Hairdreams hair extensions, there will be no damage in the application or removal of the hair extensions. Even with the hair extensions, with proper care, your natural hair will continue to grow free of damage.
+                        <p>No, I don't use glue, chemicals or any of the older methods. With <span className="bold-text"><a href="https://www.hairdreams.com/en-us/" target="_blank" rel="noopener noreferrer">Hairdreams</a></span> hair extensions, there will be no damage in the application or removal of the hair extensions. Even with the hair extensions, with proper care, your natural hair will continue to grow free of damage.
                         </p>
                         <h3>How much does it cost?</h3> 
                         <p>The price depends on what length you are looking for and what style and/or thicker or fullness you are going for.</p>
 
                         <h3>How long does it last?</h3>
-                        <p>My hair extensions stay in for up to 8 months and the hair extensions are reusable.</p>
+                        <p>My hair extensions stay in for up to <span className="bold-text">8 months</span> and the hair extensions are reusable.</p>
 
                     </div>
                     
