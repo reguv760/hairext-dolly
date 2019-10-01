ae43e5a7b51dbf04db8d3e7c6d08241a0ed0bf5f
reguv760 <reguv760@gmail.com>
2019-10-01 14:40:13 -0700

    remove rand() in Privacy component

--- src/components/Privacy.js
+++ src/components/Privacy.js
@@ -29,13 +29,11 @@ const PrivacyLinkStyle = styled.a`
     color: #e45095;
 `
 
-function rand() {
-    return Math.round(Math.random() * 20) - 10;
-}
+
 
 function getModalStyle() {
-    const top = 50 + rand();
-    const left = 50 + rand();
+    const top = 50;
+    const left = 50;
     return {
         top: `${top}%`,
         left: `${left}%`,
